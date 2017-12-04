04cde6b7eab6f87c0ddc4a7cd330c9028bdc5001
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro.local>
2017-12-04 12:23:23 -0500

    Creating new Springroll class.

---
+++ src/springroll/springroll.ts
@@ -0,0 +1,8 @@
+import { ReboundEvent } from '../rebound/rebound.interface';
+import { Rebound } from '../rebound/rebound';
+
+export class SRRebound extends Rebound {
+	constructor() {
+    	super();
+  	}
+}
